3e38d21f965ac9f2b677adad80c96b7486eb2beb
maciejriedl <maciejriedl@google.com>
2024-09-13 08:57:49 +0000

    Fix description typo

--- apis/gcpfirewall/v1/types.go
+++ apis/gcpfirewall/v1/types.go
@@ -41,7 +41,7 @@ type GCPFirewall struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
 
-	// DescriptionHello describes a GCP firewall in a human-readable way. Usually it
+	// Description describes a GCP firewall in a human-readable way. Usually it
 	// provides also an information on the producer of this GCPFirewall
 	// custom resource.
 	Description string `json:"description,omitempty"`
